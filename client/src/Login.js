ba3ba90b6ee679428bc50c7a4a45da6971eea3dc
Anis Merchant <anisxmerchant@gmail.com>
2018-12-09 11:50:13 -0500

    capstone: Implement login

---
+++ client/src/Login.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          username: '',
+        }
+    }
+  
+    submitHandler() {
+      if (this.usernameInput.value === '') {
+        alert("Pleae enter your username...");
+        return;
+      } else {
+        this.setState({username: this.usernameInput.value}, () => {
+            this.props.hideLogin(); 
+            this.props.loggedInUser(this.state.username);
+        });
+      }
+    }
+  
+    render() {
+        return (
+            <div className="main__login--containter">
+                <div className="main__login--area">
+                    <input className="main__login--name-input"
+                        name="username"
+                        type="text"
+                        className="form-control"
+                        placeholder=" Your name..."
+                        ref={input => {this.usernameInput = input}}
+                    />
+                    {/* <input
+                        type="password"
+                        className="form-control"
+                        onChange={this.handlerPasswordChange.bind(this)}
+                        ref={this.userpasswordInput}
+                    /> */}
+                    <button
+                        type="button"
+                        className="btn btn-success btn-block"
+                        onClick={this.submitHandler.bind(this)}
+                    >Login</button>
+                </div>
+            </div>
+        );
+    }
+  }
+
+  export default Login;
